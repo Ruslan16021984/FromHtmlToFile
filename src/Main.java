f7a47baed18368494e39a4f56d6eef9d9bba64a5
Ruslan16021984 <carbit3333333@gmail.com>
2019-01-29 15:58:16 +0200

    commit2

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        String urlAdress = "https://prog.kiev.ua/forum/index.php?topic=2681.120";
+        String urlAdress = "https://prog.kiev.ua/forum/index.php/topic,3841.0.html";
         String filePuth = "index.txt";
         FromLinksToFile fromLinksToFile = new FromLinksToFile(urlAdress, filePuth);
         fromLinksToFile.getLinks();
